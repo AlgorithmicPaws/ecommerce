3f396fb76114be669d5b4c365f66102d3d7f322f
Millimillan1 <camimillan2003@gmail.com>
2025-03-03 21:10:13 -0500

    Guardando cambios antes del pull

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
-import { createBrowserRouter } from 'react-router-dom'
+import Login from './pages/Login.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {path:"/",element:<App/>},
+  {path:"/login",element:<Login/>}
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+function Login(){
+    return(
+        <main>
+            <form>
+                <h1>Login Form</h1>
+            </form>
+        </main>
+    );
+}
+
+export default Login;
